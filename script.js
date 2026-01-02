d245aaa72785dd705f4da46b59b8fdc3583d759e
abraralsubaie01-wq <abrar.alsubaie01@gmail.com>
2026-01-02 17:02:43 +0300

    Update script.js

--- script.js
+++ script.js
@@ -3,3 +3,5 @@ const btn = document.getElementById("welcomeBtn");
 btn.onclick = function () {
   alert("مرحباً بك في مكتبتي الخاصة! أتمنى لك وقتاً ممتعاً.");
 };
+document.getElementById('bgVideo').play();
+
